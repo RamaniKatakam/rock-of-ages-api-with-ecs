da113033bd7480730683fac9dc99b4d7efdd2354
RamaniKatakam <ramanikatakam@gmail.com>
2026-06-24 15:44:14 -0500

    commiting changes to backend and variables file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,7 +8,7 @@ variable "aws_region" {
 variable "image_storage_bucket" {
   description = "Storage bucket for rock images created in backend-bootstrap project"
   type        = string
-  default     = "rock-of-ages-api-wth-ecs-rk" 
+  default     = "rock-of-ages-image-storing-bucket-rk" 
 }
 
 variable "db_username" {

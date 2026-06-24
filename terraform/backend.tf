ef85cdd5920dd2d9c6d84e1c4312d9abb7f992d5
RamaniKatakam <ramanikatakam@gmail.com>
2026-06-24 13:38:49 -0500

    commiting changes to backend and variables file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "<your-state-bucket-name>"  
+    bucket         = "<rock-of-ages-api-wth-ecs-rk>"  
     key            = "api/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "rock-of-ages-terraform-locks"  
